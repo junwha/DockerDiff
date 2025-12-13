209455089fa4c8f8463c57f96043f417310cdd32
Junwha Hong <qbit@unist.ac.kr>
2025-12-14 01:18:29 +0900

    Add test to deployment pipeline

--- uv/build_uv.sh
+++ uv/build_uv.sh
@@ -1,5 +1,5 @@
 PY_VERSION=$1
 TAG=junwha/ddiff-base:cu12.4.1-py${PY_VERSION}-torch-$(date +"%y%m%d")
 
-docker build -t $TAG --build-arg PY_VERSION=${PY_VERSION} ./
-docker push $TAG
+docker build -t $TAG --build-arg PY_VERSION=${PY_VERSION} ./ && \
+./test.sh $TAG && docker push $TAG

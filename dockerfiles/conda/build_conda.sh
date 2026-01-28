7e8d914f5fa33bbaff69e9e757995b8f679447ee
Junwha Hong <qbit@unist.ac.kr>
2026-01-28 18:46:12 +0900

    Add conda support

--- dockerfiles/conda/build_conda.sh
+++ dockerfiles/conda/build_conda.sh
@@ -2,5 +2,7 @@ PY_VERSION=$1
 DATE=251214 # $(date +"%y%m%d")
 TAG=junwha/ddiff-base:cu12.4.1-py${PY_VERSION}-conda-$DATE
 
-docker build -t $TAG --build-arg PY_VERSION=${PY_VERSION} ./ && \
-./test.sh $TAG && docker push $TAG
+docker build -t $TAG --build-arg PY_VERSION=${PY_VERSION} ./ 
+
+# && \
+# ./test.sh $TAG && docker push $TAG
